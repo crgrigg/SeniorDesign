6f15eed5967989a3e160051fca2001d5dd91edd6
Charles Griggs <crgrigg@g.clemson.edu>
2022-02-23 14:42:29 -0500

    Updater

---
+++ Support/Motor.py
@@ -0,0 +1,21 @@
+
+
+
+class Motor:
+
+    
+    ##VARIABLES##
+    __OBJ_ID = 0x0A
+    MotorID = -1
+
+    #Constructor
+    def __init__(self, MotorID = 1):
+       self.MotorID = MotorID
+
+    #Update Speed
+    def UpdateSpeed(self,Speed = 0):
+
+
+    #Enable
+    def EnableMotor(self):
+
