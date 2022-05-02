9bc1f9a4ab5d01cd1f82013060a74a230957dc9c
Jay Franko <71412705+jfranko1399@users.noreply.github.com>
2022-05-02 15:25:11 -0400

    Update server.py

--- TestFiles/server.py
+++ TestFiles/server.py
@@ -1,37 +1,50 @@
 # This code is for the server 
 # Lets import the libraries
-import cv2, socket, pickle, struct
+import cv2, socket, pickle, struct, imutils
 
-# Socket Create
-server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-host_name  = socket.gethostname()
-host_ip = socket.gethostbyname(host_name)
-#print('HOST IP:',host_ip)
-port = 9999
-socket_address = (("169.254.146.18", 9999))
+def read_camera():
+    
+    #cascade_src = 'cascade.xml'
+    #model_cascade = cv2.CascadeClassifier(cascade_src)
 
-# Socket Bind
-server_socket.bind(socket_address)
+    # Socket Create
+    server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    host_name  = socket.gethostname()
+    host_ip = socket.gethostbyname(host_name)
+    #print('HOST IP:',host_ip)
+    port = 9999
+    socket_address = (("0.0.0.0", 9997))
 
-# Socket Listen
-server_socket.listen(5)
-print("LISTENING AT:",socket_address)
+    # Socket Bind
+    server_socket.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+    server_socket.bind(socket_address)
 
-# Socket Accept
-while True:
-    client_socket,addr = server_socket.accept()
-    print('GOT CONNECTION FROM:',addr)
-    if client_socket:
-        vid = cv2.VideoCapture(0)
-        
-        while(vid.isOpened()):
-            img,frame = vid.read()
-            frame = imutils.resize(frame,width=640)
-            a = pickle.dumps(frame)
-            message = struct.pack("Q",len(a))+a
-            client_socket.sendall(message)
+    # Socket Listen
+    server_socket.listen(5)
+    print("LISTENING AT:",socket_address)
+
+    # Socket Accept
+    while True:
+        client_socket,addr = server_socket.accept()
+        print('GOT CONNECTION FROM:',addr)
+        if client_socket:
+            vid = cv2.VideoCapture(0)
             
-            cv2.imshow('TRANSMITTING VIDEO',frame)
-            key = cv2.waitKey(1) & 0xFF
-            if key ==ord('q'):
-                client_socket.close()
+            while(vid.isOpened()):
+                img,frame = vid.read()
+                #gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+                #model = model_cascade.detectMultiScale(gray,1.1,1)
+                #for (x,y,w,h) in model:
+                 #   cv2.rectangle(frame,(x,y),(x+w,y+h),(0,0,255),2)
+
+                frame = imutils.resize(frame,width=640)
+                a = pickle.dumps(frame)
+                message = struct.pack("Q",len(a))+a
+                client_socket.sendall(message)
+                #cv2.imshow('TRANSMITTING VIDEO',frame)
+                key = cv2.waitKey(1) & 0xFF
+                if key ==ord('q'):
+                    client_socket.close()
+
+#read_camera()
+
